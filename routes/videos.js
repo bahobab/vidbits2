91d460fffa27cdb3fbf98f7215bc6f03d231774b
mono motapa <bahobab@hotmail.com>
2018-02-02 17:25:26 -0600

    step 12: green server test

--- routes/videos.js
+++ routes/videos.js
@@ -1,9 +1,12 @@
 const router = require('express').Router();
 
+var Video = require('../models/video');
 const app = require('../app');
 
-router.post('/videos', (req, res) => {
-    res.status(201).send();
+router.post('/videos', async (req, res) => {
+    const {title, description} = req.body;
+    const video = await Video.create({title, description});
+    res.status(201).send(video);
 });
 
 module.exports = router;
