08040094d4d9d92eca9a5a1f8a197dd626f614b6
mono motapa <bahobab@hotmail.com>
2018-02-02 14:16:42 -0600

    step 11: green model tes

--- test/models/video-test.js
+++ test/models/video-test.js
@@ -1,6 +1,8 @@
 const {assert} = require('chai');
 const {mongoose, databaseUrl, options} = require('../../database');
 
+const Video = require('../../models/video');
+
 async function connectDatabase() {
   await mongoose.connect(databaseUrl, options);
   await mongoose.connection.db.dropDatabase();
@@ -14,3 +16,20 @@ module.exports = {
   connectDatabase,
   disconnectDatabase,
 }
+
+describe('MODEL', () => {
+
+  beforeEach(connectDatabase);
+  afterEach(disconnectDatabase);
+
+  it('video title is a string', async () => {
+    // set up
+    const newVideo = {
+      title: 4,
+    };
+    // exercise
+    const createdVideo = await Video.create(newVideo);
+    // assert
+    assert.strictEqual(createdVideo.title, newVideo.title.toString());
+  });
+});
