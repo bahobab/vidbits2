17f00412e06389baadcbd90eb486420fadb039f0
mono motapa <bahobab@hotmail.com>
2018-02-01 22:50:25 -0600

    step 3: red

--- test/features/user-visiting-landing-page-test.js
+++ test/features/user-visiting-landing-page-test.js
@@ -1,1 +1,14 @@
 const {assert} = require('chai');
+// const request = require('')
+
+describe('User Visit Landing page', () => {
+
+    describe('first time visit', () => {
+        it('shows empty page', async () => {
+            // set up exercise
+            browser.url('');
+            // assert
+            assert.equal(browser.getText('#videos-container'), '');
+        });
+    });
+}); 
