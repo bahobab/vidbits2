a4f11e121c226497ebb2b3c780fd28988f9dd319
mono motapa <bahobab@hotmail.com>
2018-02-03 15:02:24 -0600

    step 21: green

--- routes/videos.js
+++ routes/videos.js
@@ -18,7 +18,7 @@ router.post('/videos', async (req, res) => {
         const video = await Video.create({title, description});
         res.status(201).render('videos/show', {video});
     } else {
-        res.send('');
+        res.status(400).send('');
     }
     
 });
