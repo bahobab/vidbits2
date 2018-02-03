a5fb549b06f787a31e105dc3dfa96bc73b4fb8d2
mono motapa <bahobab@hotmail.com>
2018-02-02 20:54:28 -0600

    step 15-16: refactor

--- routes/videos.js
+++ routes/videos.js
@@ -3,10 +3,14 @@ const router = require('express').Router();
 var Video = require('../models/video');
 const app = require('../app');
 
+router.get('/', (req, res) => {
+    res.render('index');
+});
+
 router.post('/videos', async (req, res) => {
     const {title, description} = req.body;
     const video = await Video.create({title, description});
-    res.status(201).send(video);
+    res.status(201).render('videos/show', {video});
 });
 
 module.exports = router;
