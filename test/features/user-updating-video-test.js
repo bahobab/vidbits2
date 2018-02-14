fb9660bc99761bbddab5282e29ae6ed3406217e4
mono motapa <bahobab@hotmail.com>
2018-02-13 20:06:06 -0600

    step 38 : red

---
+++ test/features/user-updating-video-test.js
@@ -0,0 +1,35 @@
+const {assert} = require('chai');
+const {jsdom} = require('jsdom');
+
+const {fillForm} = require('./formUtil');
+const generateRandomUrl = (domain) => {
+    return `http://${domain}/${Math.random()}`
+};
+
+
+describe('Updating video', () => {
+    it('click on a button navigate to edit page', () => {
+        // set up
+        const newVideo = {
+            title: 'My Kool Video',
+            videoUrl: generateRandomUrl('mydomain'),
+            description: 'Rare Lunar Eclipse'
+        };
+        // exercise
+        browser.url('/videos/create');  // (1)
+        fillForm(browser, newVideo.title, newVideo.videoUrl, newVideo.description);             // 2
+        
+        // redirected to videos/show (3)
+        browser.click('#edit-video');
+        // redirected to /videos/edit (4)
+        // new video title
+        const newTitle = 'My Very Kool Video';
+        // fill out form/submit
+        fillForm(browser, newTitle, browser.getText('iframe').src, browser.getText('#video-description'));
+        // redirected back to /videos/show (5)
+        
+        // assert
+        assert.include(browser.getText('body'), newTitle);
+
+    });
+});
