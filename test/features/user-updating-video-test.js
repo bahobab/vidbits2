3417ef8e0ad2d1842d7a6be592594d3792e29f36
mono motapa <bahobab@hotmail.com>
2018-02-14 21:39:43 -0600

    step 40 : revisited: implement assert 'updating video does not create additional video - old title not present on page'

--- test/features/user-updating-video-test.js
+++ test/features/user-updating-video-test.js
@@ -33,7 +33,27 @@ describe('Updating video', () => {
         assert.include(browser.getText('body'), 'Update a video');
     });
 
-    it('updating video does not create another video', () => {
+    it('updating video does not create another video', async () => {
+        // set up
+        const newVideo = {
+            title: 'My Kool Video',
+            videoUrl: generateRandomUrl('mydomain'),
+            description: 'Rare Lunar Eclipse'
+        };
+        // exercise
+        browser.url('/videos/create');  // (1)
+        fillForm(browser, newVideo.title, newVideo.videoUrl, newVideo.description);             // 2
+        
+        // redirected to videos/show (3)
+        browser.click('#edit-video');
+        // redirected to /videos/edit (4)
+        // new video title
+        const newTitle = 'My Very Kool Video';
+        // fill out form/submit
+        fillForm(browser, newTitle, browser.getAttribute('#title-input', 'value'), browser.getText('#description-input'));
+        // redirected back to /videos/show (5)
 
+        // assert
+        assert.notInclude(browser.getAttribute('#title-input', 'value'), newVideo.title);
     });
 });
